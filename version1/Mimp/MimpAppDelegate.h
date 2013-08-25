2e22e44abe2d2f53f921ddbfa34ebaa1a53bf4e9
sota0805 <sota.yamashita@gmail.com>
2013-08-26 01:35:57 +0900

    add version0.1

---
+++ version1/Mimp/MimpAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MimpAppDelegate.h
+//  Mimp
+//
+//  Created by Sota Yamashita on 8/25/13.
+//  Copyright (c) 2013 Sota Yamashita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MimpAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ version1/Mimp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Mimp
+//
+//  Created by Sota Yamashita on 8/25/13.
+//  Copyright (c) 2013 Sota Yamashita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MimpAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MimpAppDelegate class]));
+    }
+}
